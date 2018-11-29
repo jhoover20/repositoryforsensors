52c4b3f6783c42fcc52cd695fb18f439b992c5a8
jhoover20 <44002812+jhoover20@users.noreply.github.com>
2018-11-29 12:52:41 -0600

    Update elif.py

--- elif.py
+++ elif.py
@@ -1,1 +1,12 @@
-
+import RoboPiLib as RPL
+import setup
+x = 1
+Sensor_pin = 16
+Sensor_pin = 15
+RPL.pinMode(sensor_pin,RPL.INPUT)
+while x == 1:
+    reading = RPL.digitalRead(sensor_pin)
+    if reading == 0:
+        RPL.servoWrite(0, 1600)
+    elif reading == 1:
+        RPL.servoWrite(0, 300)
