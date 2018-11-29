9560f9ba22a77734ad6303e4479d9bb74694c892
jhoover20 <44002812+jhoover20@users.noreply.github.com>
2018-11-29 12:46:13 -0600

    Update &.py

--- &.py
+++ &.py
@@ -1,1 +1,13 @@
-
+import RoboPiLib as RPL
+import setup
+x = 1
+Sensor_pin = 16
+Sensor_pin = 15
+RPL.pinMode(sensor_pin,RPL.INPUT)
+while x == 1:
+    reading = RPL.digitalRead(16)
+    reading = RPL.digitalRead(15)
+    if reading == 0:
+        RPL.servoWrite(0, 1600)
+    elif reading == 1:
+        RPL.servoWrite(0, 1500)
